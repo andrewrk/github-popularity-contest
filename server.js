4dfa5e764f14630b41d8e94b94d2c6c9710f7b29
Andrew Kelley <superjoe30@gmail.com>
2013-07-19 15:13:43 -0400

    use mongodb to save state and use less memory

--- server.js
+++ server.js
@@ -8,23 +8,24 @@ var consolidate = require('consolidate');
 var swig = require('swig');
 var assert = require('assert');
 var http = require('http');
+var mongo = require('mongodb');
+var MongoClient = mongo.MongoClient;
 
-// collected data
-var users = {};
-var repos = {};
+var db;
+var reposCollection;
+var metaCollection;
 
 // cached/calculated data
 var goodUsers = [];
 var top100 = [];
-var userPoints = {};
-var userContribCount = {};
 var userCount = 0;
 var repoCount = 0;
 var contribCount = 0;
 var recentlyCrawled = [];
+var calculating = false;
 
 var nextRefresh = null;
-var REFRESH_INTERVAL = 2000;
+var REFRESH_INTERVAL = 4000;
 var MIN_POINT_THRESHOLD = 10;
 var MAX_RECENTLY_CRAWLED = 20;
 
@@ -65,20 +66,28 @@ app.post('/crawl', express.bodyParser(), function(req, resp) {
     resp.render('crawl', {error: 'invalid repoId: ' + repoId});
     return;
   }
-  var repo = repos[repoId];
-  if (repo != null) {
-    var ONE_HOUR = 1000 * 60 * 60;
-    var diff = new Date() - repo.crawlDate;
-    if (diff < ONE_HOUR ) {
-      var minutesAgo = Math.round(diff / (1000 * 60));
+  reposCollection.findOne({full_name: repoId}, function(err, repo) {
+    if (err) {
       resp.render('crawl', {
-        error: "that repo was last crawled " + minutesAgo + " minutes ago.",
+        error: "error finding repo: " + err.message,
       });
       return;
     }
-  }
-  crawlQueue.unshift(repoId);
-  resp.render('crawl');
+    if (repo != null) {
+      var ONE_HOUR = 1000 * 60 * 60;
+      var diff = new Date() - repo.crawlDate;
+      if (diff < ONE_HOUR ) {
+        var minutesAgo = Math.round(diff / (1000 * 60));
+        resp.render('crawl', {
+          error: "that repo was last crawled too recently: " +
+            minutesAgo + " minutes ago.",
+        });
+        return;
+      }
+    }
+    state.crawlQueue.unshift(repoId);
+    resp.render('crawl');
+  });
 });
 
 app.get('/find', function(req, resp) {
@@ -114,29 +123,50 @@ app.get('/find', function(req, resp) {
 var server = http.createServer(app);
 var port = process.env.PORT || 25751;
 var host = process.env.HOST || "0.0.0.0";
-server.listen(port, host, function() {
-  console.log(process.env.NODE_ENV + " listening at http://" + host + ":" + port + "/");
+var state = {
+  id: 'meta',
+  lastId: "",
+  crawlQueue: [],
+};
+
+
+var dbUrl = "mongodb://127.0.0.1:27017/gh_pop_contest";
+MongoClient.connect(dbUrl, function(err, mongoDb) {
+  if (err) {
+    console.error("Unable to connect to mongodb:", err.stack);
+    return;
+  }
+  db = mongoDb;
+  reposCollection = db.collection('repositories');
+  metaCollection = db.collection('meta');
+
+  loadState(function(err) {
+    if (err) {
+      console.error("Error loading state:", err.stack);
+      return;
+    }
+    crawlNextRepo();
+    setInterval(crawlNextRepo, 3000);
+    server.listen(port, host, function() {
+      console.log(process.env.NODE_ENV + " listening at http://" + host + ":" + port + "/");
+    });
+  });
 });
 
-var crawlQueue = [];
-var lastId = "";
-
-crawlNextRepo();
-setInterval(crawlNextRepo, 3000);
-
 function crawlNextRepo() {
-  var repoId = crawlQueue.shift();
+  var repoId = state.crawlQueue.shift();
   if (repoId) {
     crawlRepo(repoId, function(err) {
       if (err) {
         console.error("error crawling", err.stack);
         return;
       }
+      saveState();
       console.log("crawled", repoId);
     });
   } else {
     // need to get next page of index
-    ghApiGet("/repositories?since=" + lastId, function(err, allRepoPage) {
+    ghApiGet("/repositories?since=" + state.lastId, function(err, allRepoPage) {
       if (err) {
         console.error("error listing repositories:", err.stack);
         return;
@@ -144,14 +174,15 @@ function crawlNextRepo() {
       if (allRepoPage.length === 0 ) {
         // start over
         console.log("reached the end, starting over.");
-        lastId = "";
+        state.lastId = "";
         return;
       }
       for (var i = 0; i < allRepoPage.length; i += 1) {
         var thisRepo = allRepoPage[i];
-        lastId = thisRepo.id;
-        crawlQueue.push(thisRepo.full_name);
+        state.lastId = thisRepo.id;
+        state.crawlQueue.push(thisRepo.full_name);
       }
+      saveState();
       console.log("found", allRepoPage.length, "repositories");
     });
   }
@@ -159,63 +190,81 @@ function crawlNextRepo() {
 
 function scheduleRefreshCalculations() {
   if (nextRefresh != null) return;
-  nextRefresh = setTimeout(function() {
-    nextRefresh = null;
-    refreshCalculations();
-  }, REFRESH_INTERVAL);
+  nextRefresh = setTimeout(maybeRefresh, REFRESH_INTERVAL);
+
+  function maybeRefresh() {
+    if (calculating) {
+      nextRefresh = setTimeout(maybeRefresh, REFRESH_INTERVAL);
+    } else {
+      nextRefresh = null;
+      refreshCalculations();
+    }
+  }
 }
 
 function refreshCalculations() {
-  userPoints = {};
-  userContribCount = {};
-  repoCount = 0;
-  userCount = 0;
-  contribCount = 0;
+  calculating = true;
 
-  var i;
-  for (var repoId in repos) {
-    repoCount += 1;
-    var repo = repos[repoId];
-    var totalContribs = 0;
-    var contributor;
-    for (i = 0; i < repo.contributors.length; i += 1) {
-      contributor = repo.contributors[i];
-      totalContribs += contributor.contributions;
-      contribCount += contributor.contributions;
+  var newRepoCount = 0;
+  var newUserCount = 0;
+  var newContribCount = 0;
+  var newUserData = {};
+
+  var cursor = reposCollection.find({});
+  cursor.nextObject(handleNext);
+
+  function handleNext(err, repo) {
+    if (err) {
+      console.error("error doing calculations:", err.stack);
+      return;
     }
-    repo.totalContribs = totalContribs;
-    if (totalContribs === 0) continue;
-    for (i = 0; i < repo.contributors.length; i += 1) {
-      contributor = repo.contributors[i];
-      var ownership = contributor.contributions / totalContribs;
-
-      var prevPoints = userPoints[contributor.login];
-      if (prevPoints == null) prevPoints = 0;
-      userPoints[contributor.login] = prevPoints + ownership * repo.watchers;
-
-      var prevContribCount = userContribCount[contributor.login];
-      if (prevContribCount == null) prevContribCount = 0;
-      userContribCount[contributor.login] = prevContribCount + contributor.contributions;
+    if (repo == null) {
+      processUsers();
+      return;
     }
+
+    newRepoCount += 1;
+    newContribCount += repo.totalContribs;
+    for (var i = 0; i < repo.contributors.length; i += 1) {
+      var contributor = repo.contributors[i];
+      var ownership = contributor.contributions / repo.totalContribs;
+
+      var userData = newUserData[contributor.login];
+      if (userData == null) {
+        userData = contributor;
+        userData.score = 0;
+        userData.contribCount = 0;
+      }
+
+      userData.score += ownership * repo.watchers;
+      userData.contribCount += contributor.contributions;
+
+      newUserData[contributor.login] = userData;
+    }
+    cursor.nextObject(handleNext);
   }
 
-  // all users above a certain threshold
-  goodUsers = [];
-  for (var uid in userPoints) {
-    userCount += 1;
-    var score = userPoints[uid];
-    users[uid].score = score;
-    users[uid].contribCount = userContribCount[uid];
-    if (score > MIN_POINT_THRESHOLD) {
-      goodUsers.push(users[uid]);
+  function processUsers() {
+    // all users above a certain threshold
+    goodUsers = [];
+    for (var uid in newUserData) {
+      newUserCount += 1;
+      var user = newUserData[uid];
+      if (user.score > MIN_POINT_THRESHOLD) goodUsers.push(user);
     }
-  }
-  goodUsers.sort(function(a, b) {
-    return b.score - a.score;
-  });
-  top100 = [];
-  for (i = 0; i < 100 && i < goodUsers.length; i += 1) {
-    top100.push(goodUsers[i]);
+    goodUsers.sort(function(a, b) {
+      return b.score - a.score;
+    });
+    top100 = [];
+    for (var i = 0; i < 100 && i < goodUsers.length; i += 1) {
+      top100.push(goodUsers[i]);
+    }
+
+    repoCount = newRepoCount;
+    userCount = newUserCount;
+    contribCount = newContribCount;
+
+    calculating = false;
   }
 }
 
@@ -248,28 +297,32 @@ function crawlRepo(repoId, cb) {
     }
     var repo = results[0];
     var contribs = results[1];
-    repo.contributors = contribs;
-    repos[repoId] = repo;
 
-    repo.crawlDate = new Date();
+    var totalContribs = 0;
+    for (var i = 0; i < contribs.length; i += 1) {
+      totalContribs += contribs[i].contributions;
+    }
 
-    // add the user to our cached data
-    contribs.forEach(function(contrib) {
-      var user = users[contrib.login];
-      if (user == null) {
-        user = {};
-        users[contrib.login] = user;
-      }
-      user.login = contrib.login;
-      user.id = contrib.id;
-      user.avatar_url = contrib.avatar_url;
-      user.gravatar_id = contrib.gravatar_id;
-      user.url = contrib.url;
-    });
+    var repoData = {
+      full_name: repo.full_name,
+      contributors: contribs,
+      crawlDate: new Date(),
+      watchers: repo.watchers,
+      html_url: repo.html_url,
+      totalContribs: totalContribs,
+    };
 
-    scheduleRefreshCalculations();
+    if (totalContribs > 0 && repo.watchers > 0) {
+      reposCollection.update({full_name: repoId}, repoData, {upsert: true}, function(err) {
+        if (err) {
+          console.error("error adding repo to db:", err.stack);
+        }
+      });
 
-    recentlyCrawled.unshift(repo);
+      scheduleRefreshCalculations();
+    }
+
+    recentlyCrawled.unshift(repoData);
     if (recentlyCrawled.length > MAX_RECENTLY_CRAWLED) {
       recentlyCrawled.pop();
     }
@@ -292,4 +345,21 @@ function ghApiGet(urlPath, cb) {
       cb(new Error(url + " response status " + resp.status + ": " + resp.text));
     }
   });
+}
+
+function loadState(cb) {
+  metaCollection.findOne({id: state.id}, function(err, newState) {
+    if (err) return cb(err);
+    if (newState != null) state = newState;
+    cb();
+  });
+}
+
+function saveState() {
+  metaCollection.update({id: state.id}, state, {upsert: true}, cb);
+  function cb(err) {
+    if (err) {
+      console.error("Error saving state:", err.stack);
+    }
+  }
 }
